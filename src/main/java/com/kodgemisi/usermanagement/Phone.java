d9f97b14cbde3f1f24c68d0fbde3fd009a6c69a0
Bertuğ Kara <bertugkara@outlook.com>
2022-05-30 18:01:52 +0200

    Second Commit, Phone Validation added, test cases are 7/8 working, Unexcepted exception type thrown will be solved.

--- src/main/java/com/kodgemisi/usermanagement/Phone.java
+++ src/main/java/com/kodgemisi/usermanagement/Phone.java
@@ -1,14 +1,58 @@
 package com.kodgemisi.usermanagement;
 
 /**
- * Represents a phone number, never stores a null value. If constructed with a null value stores an empty string.
+ * Represents a phone number, never stores a null value. If constructed with a
+ * null value stores an empty string.
  */
 public class Phone implements CharSequence {
 
-	private final String phoneNumber;
+	private String phoneNumber;
+	public final boolean verified;
 
 	public Phone(String phoneNumber) {
-		this.phoneNumber = phoneNumber == null ? "" : phoneNumber;
+
+		this.phoneNumber = phoneNumber;
+
+		validate(phoneNumber);
+
+		this.verified = false;
+	}
+
+	public Phone(String phoneNumber, boolean verified) {
+
+		validate(phoneNumber);
+
+		this.verified = verified;
+	}
+
+	// PHONE NUMBER VALIDATION
+	
+	public boolean validate(String phoneNumber) {
+		final String areaCodePlusSign = "+";
+
+		if (phoneNumber.equals("112") || phoneNumber.equals("911")) {
+			return true;
+		}
+		else if (phoneNumber==null ) {
+			throw new IllegalArgumentException("A phone number cannot be null");
+		}
+		else if ( phoneNumber.isBlank() ) {
+			throw new IllegalArgumentException("Phone Number cannot be blank");
+		}
+		else if (!String.valueOf(phoneNumber).startsWith(areaCodePlusSign)) {
+			throw new IllegalArgumentException(
+					"Invalid Phone Number, There Are No Plus Sign: '%s'!".formatted(phoneNumber));
+		}
+		else if (String.valueOf(phoneNumber).startsWith(areaCodePlusSign) && phoneNumber.length() < 5) {
+			throw new IllegalArgumentException(
+					"Invalid Phone Number, If you are starting with Plus Sign, Your number must contains least 5 char long: '%s'!"
+							.formatted(phoneNumber));
+		} else if (String.valueOf(phoneNumber).startsWith("+90") && phoneNumber.length() != 13) {
+			throw new IllegalArgumentException(
+					"Invalid Phone Number If you are starting with +90 prefix, Your number must contains at least 13 char long: '%s'!"
+							.formatted(phoneNumber));
+		}
+		return true;
 	}
 
 	public String number() {
@@ -37,8 +81,10 @@ public class Phone implements CharSequence {
 
 	@Override
 	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
 
 		Phone phone = (Phone) o;
 
